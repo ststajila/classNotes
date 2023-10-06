d664cfe8a67084a132ccb8117a9ad9608efe870f
Stanislav <stazilastas@gmail.com>
2023-10-06 09:16:06 -0500

    Initial Commit

---
+++ classNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classNotes
+//
+//  Created by STANISLAV STAJILA on 10/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
